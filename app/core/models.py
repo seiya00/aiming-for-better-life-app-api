aca179ea50f0a06095a8facd263e7647fc8a59ef
seiya00 <seiyadailylife@gmail.com>
2023-03-26 21:21:40 +0900

    create custom user model and test

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,44 @@
-from django.db import models # noqa
+"""
+Database model
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    GENDER_CHOICES = [
+        ('male', '男性'),
+        ('female', '女性'),
+        ('other', 'その他')
+    ]
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    # birth_date = models.DateField(null=False)
+    # user_name = models.CharField(max_length=255, unique=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_family = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
