20eeb6305ba90e79b95642333ddba1d5f68594d3
seiya00 <seiyadailylife@gmail.com>
2023-04-01 13:18:54 +0900

    add views.py file for Meal app

--- app/meal/views.py
+++ app/meal/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+"""
+Views for the Meal APIs
+"""
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from core.models import MealQuestion, MealUser
+from meal import serializers
+
+
+class MealQuestionViewSet(viewsets.ModelViewSet):
+    """View for manage meal question APIs"""
+    serializer_class = serializers.MealQuestionSerializer
+    queryset = MealQuestion.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve meal question for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
